cd592b0b501d50116b6298a0c07da8aa766769e7
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-06-29 04:47:16 +0100

    style(preloader): center preloader vertically and horizontally on the page using flexbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { AnimatedPinDemo } from "./components/AnimatePinDemo";
+import Preloader from "./components/Preloader";
 
 const App = () => {
   return (
-    <>
-      <AnimatedPinDemo />
-    </>
+    <div className="flex items-center justify-center min-h-screen w-full">
+      <Preloader
+        sentence="One sec, please..."
+        manualMode={false}
+        blurAmount={5}
+        borderColor="red"
+        animationDuration={2}
+        pauseBetweenAnimations={1}
+      />
+    </div>
   );
 };
 
